36e973eff277bd73b9117a79a51d4ae0897b4d55
pkale <65419691+devprnvk@users.noreply.github.com>
2024-07-16 21:44:49 -0400

    [Implement] Train/Classifier devprnvk

--- BlackJack/src/model.py
+++ BlackJack/src/model.py
@@ -17,3 +17,12 @@ X = data[features]
 y = data[target]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier()
+
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+print('Accuracy:', accuracy_score(y_test, predictions))
+
+joblib.dump(model, 'blackjack_ai_model.pkl')
